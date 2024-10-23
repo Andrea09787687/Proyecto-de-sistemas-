199b50cf02b1d133b3c13993e31d7758a45b87de
AndresValero <valerocomingsoon@gmail.com>
2024-10-23 00:20:35 -0500

    añadiendo estructura y .gitignore

---
+++ src/server/routes/reactions.js
@@ -0,0 +1,26 @@
+// reactions.js
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.reaction-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const reaction = button.getAttribute('data-reaction');
+            
+            // Hacer una solicitud al servidor
+            fetch('/guardar-reaccion', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ reaction: reaction }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Reacción guardada:', data);
+            })
+            .catch((error) => {
+                console.error('Error al guardar la reacción:', error);
+            });
+        });
+    });
+});
